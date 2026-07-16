12cc3bde8777af6e8225c523ee90085ee6a48027
Dandares10 <a.danijoshua10@gmail.com>
2026-07-16 14:04:36 +0530

    Add shuffle method to rearrange array
    Implement shuffle method to rearrange array elements. Also, I have various other approaches to solving this problem and would be posting it below the current codes.

---
+++ Day 3 [1470]. Shuffle the Array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+       int[] r = new int[nums.length];
+       int e = 0;
+
+       for(int i =0; i<n; i++){
+        r[e++] = nums[i];
+        r[e++] = nums[i+n];
+       }
+       return r;
+            
+        
+    }
+}
