7535bdd05749ba0138783adaaada837f6b4402db
Dandares10 <a.danijoshua10@gmail.com>
2026-07-16 14:17:47 +0530

    Enhance comments in shuffle method for better understanding
    Added comments for clarity in the shuffle method.

--- Day 3 [1470]. Shuffle the Array.java
+++ Day 3 [1470]. Shuffle the Array.java
@@ -1,14 +1,15 @@
-class Solution {
+    class Solution {
     public int[] shuffle(int[] nums, int n) {
-       int[] r = new int[nums.length];
-       int e = 0;
+       int[] r = new int[nums.length];   //creating a new array result
+       int e = 0; //elements
 
        for(int i =0; i<n; i++){
-        r[e++] = nums[i];
-        r[e++] = nums[i+n];
+        r[e++] = nums[i];   //it takes the first element
+        r[e++] = nums[i+n];   // since the whole array is 2n, adding it with i creates the next element after i
        }
        return r;
             
         
     }
-}
+}/* at this point, there might be a feeling of not getting it, and that's because of i+n; since, 
+we are accessing it we subconsciously think the array is going out of thr assigned limit because we forget that the array is of size 2n */ 
